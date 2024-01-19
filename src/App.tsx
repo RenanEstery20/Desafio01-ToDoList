037a23145b786a7520dc1a595bdff278f8788818
Renan Estery <renan.estery@compasso.com.br>
2024-01-19 14:23:59 -0300

    feature_initialComponents: created basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import styles from './App.module.css';
+import { Button } from './components/Button';
+import { Header } from './components/Header';
+import { Input } from './components/Input';
+import { Header as ListHeader } from './components/List/Header';
+import { Item } from './components/List/Item';
 export function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Header />
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <Input />
+          <Button></Button>
+        </div>
+        <div className={styles.tasksList}>
+          <ListHeader />
+          <div>
+            <Item />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
